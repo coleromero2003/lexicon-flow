3ed2b92022e36528b5e66cfb8c16c253452be083
Cole Romero <coleromero2003@gmail.com>
2025-10-02 17:00:17 -0600

    Fixing unused modules and string type errors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -26,7 +26,6 @@ import {
   Filter,
   Grid3x3,
   List,
-  Loader2,
   Plus,
   Rocket,
   Search,
@@ -38,7 +37,7 @@ import { useState } from "react";
 
 export default function DashboardPage() {
   const { user } = useUser();
-  const { createBoard, boards, loading, error } = useBoards();
+  const { createBoard, boards, error } = useBoards();
   const router = useRouter();
   const { isFreeUser } = usePlan();
   const [viewMode, setViewMode] = useState<"grid" | "list">("grid");
@@ -128,7 +127,7 @@ export default function DashboardPage() {
             {user?.firstName ?? user?.emailAddresses[0].emailAddress}! 👋
           </h1>
           <p className="text-gray-600">
-            Here's what's happening with your boards today.
+            Here&apos;s what&apos;s happening with your boards today.
           </p>
         </div>
 
