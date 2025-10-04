30181d5b1bb90771536d559c3a533d81acc5e26e
Cole Romero <coleromero2003@gmail.com>
2025-10-04 17:26:00 -0600

    fixing supabase url and anon key in provider

--- lib/supabase/SupabaseProvider.tsx
+++ lib/supabase/SupabaseProvider.tsx
@@ -25,8 +25,8 @@ export default function SupabaseProvider({
   useEffect(() => {
     if (!session) return;
     const client = createClient(
-      process.env.SUPABASE_URL!,
-      process.env.SUPABASE_ANON_KEY!,
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         accessToken: () => session?.getToken(),
       }
